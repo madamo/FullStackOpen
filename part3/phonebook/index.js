270fdd389f950f1dc91db8552c15b28d1fd76c09
madamo <mattadamo@gmail.com>
2025-05-08 13:05:12 -0700

    part3 exercises 3_15-18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -12,7 +12,7 @@ app.use(express.static('dist'))
 
 //TO-DO: extra credit - configure morgan so that it also shows the data sent in HTTP POST requests
 
-let persons = [
+/*let persons = [
     { 
         "id": "1",
         "name": "Arto Hellas", 
@@ -33,7 +33,7 @@ let persons = [
         "name": "Mary Poppendieck", 
         "number": "39-23-6423122"
     }
-]
+]*/
 
 app.get('/info', (request, response) => {
     const time = new Date()
@@ -51,20 +51,32 @@ app.get('/api/persons', (request, response) => {
     Person.find({}).then(people => {
         response.json(people)
     })
-    //response.json(persons)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-   Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+   Person.findById(request.params.id)
+    .then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
    })
+   .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
+app.delete('/api/persons/:id', (request, response, next) => {
+   Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+
+   
+    /* const id = request.params.id
     persons = persons.filter(person => person.id !== id)
 
-    response.status(204).end()
+    response.status(204).end()*/
 })
 
 app.post('/api/persons', (request, response) => {
@@ -100,6 +112,39 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+    console.log(name, number)
+
+    Person.findById(request.params.id)
+        .then(person => {
+            if (!person) {
+                console.log('person not found')
+                return response.status(404).end()
+            }
+
+            person.name = name
+            person.number = number
+
+            return person.save().then((updatedPerson) => {
+                response.json(updatedPerson)
+            })
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformed id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
-console.log(url)
 
 console.log('connecting to', url)
 
