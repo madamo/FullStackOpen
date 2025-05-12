b14517e9698dcb3fcedf2581677071abe4d329f4
madamo <mattadamo@gmail.com>
2025-05-11 20:53:07 -0700

    part3 validation added

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -80,6 +80,9 @@ function App() {
             setMessageType(null)
           }, 2000)
         })
+        .catch(error => {
+          console.log(error.response.data.error)
+        })
     }
   }
 
