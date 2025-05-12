3c9874abeb262e57a2650ded3bcfa8d756b376d4
madamo <mattadamo@gmail.com>
2025-05-12 12:56:09 -0700

    part4 bloglist initial commit

---
+++ part4/bloglist/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const password = process.argv[2]
+console.log(password)
+
+const mongoUrl = `mongodb+srv://mattadamoux:${password}@cluster0.u3sqg.mongodb.net/bloglistApp?retryWrites=true&w=majority&appName=Cluster0`
+
+
+//const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
