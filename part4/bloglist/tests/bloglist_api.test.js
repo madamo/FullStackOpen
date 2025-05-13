b189cc6f9b36b28e2acc21f7a6056659cf617dfd
madamo <mattadamo@gmail.com>
2025-05-13 13:43:20 -0700

    part4 bloglist 4_8

---
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -0,0 +1,32 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
+
