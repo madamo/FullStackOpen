1749eaf6081914c8262bef7f030d411a35622411
madamo <mattadamo@gmail.com>
2025-05-19 09:00:14 -0700

    part4 bloglist 4_15

---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+//TO-DO: Add blogs reference to user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: String,
+  passwordHash: String
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the password hash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
+
