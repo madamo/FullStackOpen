add6895a202e4760dccbe60e97438943eb0e00a9
madamo <mattadamo@gmail.com>
2025-05-21 15:53:03 -0700

    part5 bloglist-frontend 5_1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,26 +1,60 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
  useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
+    ).catch(error => {
+      console.log(error)
+    })
+  }, [user])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with ', username, password)
+    try {
+      const loggedInUser = await loginService.login({ username, password })
+      blogService.setToken(loggedInUser.token)
+      setUser(loggedInUser)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <Login 
+          submitHandler={handleLogin}
+          username={username} 
+          updateUsername={setUsername} 
+          password={password}
+          updatePassword={setPassword} 
+        />
+      </div> 
     )
-  }, [])
+  }
 
   return (
     <div>
       <h2>Blogs</h2>
       {blogs.map(blog => 
-      <Blog key={blog.id} blog={blog} /> )}
+        <Blog key={blog.id} blog={blog} /> 
+      )}
     </div>
   )
 }
-
-
-
 
 export default App
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,21 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = async () => {
-  const response = await axios.get(baseUrl)
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.get(baseUrl, config)
   return response.data
 }
 
-export default { getAll }
+export default { 
+  getAll,
+  setToken
+}
