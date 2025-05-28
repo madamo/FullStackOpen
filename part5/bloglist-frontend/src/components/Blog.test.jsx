b6aefbe177ae619a75da70e60d8636749e46be66
madamo <mattadamo@gmail.com>
2025-05-28 14:43:56 -0700

    part5 bloglist-frontend 5_14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -23,13 +24,20 @@ describe('<Blog />', () => {
 
 
   test('renders title and author by default, but not blog details', () => {
-
-  //render(<Blog blog={blog} />)
-
     const blogInfo = container.querySelector('.title-author')
     const blogDetails = container.querySelector('.blog-details')
     expect(blogInfo).not.toHaveStyle('display: none')
     expect(blogDetails).toHaveStyle('display: none')
-
   })
+
+  test('renders url and likes when user clicks show', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show')
+
+    await user.click(button)
+
+    const blogDetails = container.querySelector('.blog-details')
+    expect(blogDetails).not.toHaveStyle('display: none')
+  })
+
 })
