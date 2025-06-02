68f2c19f8dd76dca0e87bca1f70dce355d0f8c47
madamo <mattadamo@gmail.com>
2025-06-02 16:59:31 -0700

    part6 redux-anecdotes 6_3-5

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,52 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch({ 
+      type: 'VOTE', 
+      payload: {
+        id: id
+      }
+    })
+  }
+
+  const addNew = (event) => {
+    event.preventDefault()
+    console.log("textbox value:", event.target.anecdote.value)
+    dispatch({ 
+      type: 'ADD_NEW', 
+      payload: {
+        content: event.target.anecdote.value
+      }
+    })
+    event.target.anecdote.value = ''
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={addNew}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ part6/redux-anecdotes/src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
+
+const store = createStore(reducer)
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
