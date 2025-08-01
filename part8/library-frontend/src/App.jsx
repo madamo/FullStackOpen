2e003c9e4b84e4b6ea87ff5ceb3a921c92883745
madamo <mattadamo@gmail.com>
2025-08-01 09:39:42 -0700

    part8 library-frontend 8_18 setup login form

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -3,12 +3,17 @@ import { BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
+import { useState } from 'react'
 
 import Authors from './components/Authors'
 import Books from './components/Books'
+import LoginForm from './components/LoginForm'
 import NewBook from './components/NewBook'
 
 const App = () => {
+
+  const [token, setToken] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const linkStyle = {
     padding: 5,
@@ -22,12 +27,14 @@ const App = () => {
         <Link to="/" style={linkStyle}>authors</Link>
         <Link to="/books" style={linkStyle}>books</Link>
         <Link to="/add-book" style={linkStyle}>add book</Link>
+        <Link to="/login" style={linkStyle}>login</Link>
       </div>
 
       <Routes>
         <Route path="/" element={<Authors />} />
         <Route path="/books" element={<Books />} />
         <Route path="/add-book" element={<NewBook />} />
+        <Route path="/login" element={<LoginForm setToken={setToken} /> }/>
       </Routes>
   
     </Router>
