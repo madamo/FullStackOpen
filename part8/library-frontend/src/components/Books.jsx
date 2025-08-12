65edd8fcdd0b75a0b8882a96f61ad48ea7a98458
madamo <mattadamo@gmail.com>
2025-08-12 10:16:57 -0700

    part8 library-frontend 8_21

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -3,50 +3,49 @@ import { useQuery } from "@apollo/client"
 import { ALL_BOOKS } from "../queries"
 
 const Books = () => {
-  
-  const result = useQuery(ALL_BOOKS)
 
-  const genres = []
-  const [booksToShow, setBooksToShow] = useState([])
-  const [filter, setFilter] = useState('all')
+  const [genres, setGenres] = useState([])
+  //const [booksToShow, setBooksToShow] = useState([])
+  const [filter, setFilter] = useState(null)
 
+  const result = useQuery(ALL_BOOKS, {
+    variables: { genre: filter }
+  })
 
   if (result.loading) {
     return <div>LOADING...</div>
   }
 
+  console.log(result)
+
   const getGenres = (books) => {
+    // Create list of genres, then update state
+    const libraryGenres = []
     books.map(book => {
+      console.log(book)
       book.genres.forEach(genre => {
-        if (!genres.includes(genre)) {
-          genres.push(genre)
+        if (!libraryGenres.includes(genre)) {
+          libraryGenres.push(genre)
         }
       })
     })
+    setGenres(libraryGenres)
   }
 
   const filterBooks = (genre) => {
-    console.log('filtering by', genre)
     setFilter(genre)
-    if (genre === 'all') {
-      setBooksToShow(result.data.allBooks)
-    } else {
-      const filteredBooks = result.data.allBooks.filter(book => 
-        book.genres.includes(genre)
-      )
-
-      setBooksToShow(filteredBooks)
-      console.log(booksToShow)
-    }
   }
 
-  getGenres(result.data.allBooks)
+  if (genres.length === 0) {
+    console.log('getting genres...')
+    getGenres(result.data.allBooks)
+  }
 
 
   return (
     <div>
       <h2>books</h2>
-        <div>showing books in genre: {filter}</div>
+        <div>showing books in genre: {filter ? filter : 'all'}</div>
 
       <table>
         <tbody>
@@ -55,7 +54,7 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {booksToShow.map((a) => (
+          {result.data.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -67,7 +66,7 @@ const Books = () => {
       {genres.map(g => 
         <button key={g} onClick={() => filterBooks(g)}>{g}</button>
       )}
-      <button onClick={() => filterBooks('all')}>all genres</button>
+      <button onClick={() => filterBooks(null)}>all genres</button>
     </div>
   )
 }
