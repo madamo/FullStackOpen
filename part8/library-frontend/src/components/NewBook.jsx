6fe93ca56519a58f3eb81a5958c47b45b48e3ea5
madamo <mattadamo@gmail.com>
2025-08-22 16:07:50 -0700

    part8 library-frontend 8_25

--- part8/library-frontend/src/components/NewBook.jsx
+++ part8/library-frontend/src/components/NewBook.jsx
@@ -21,8 +21,25 @@ const NewBook = ({ setError }) => {
     },
     update: (cache, response) => {
       //TO-DO: Update cache
+      updateCache(cache, { query: ALL_BOOKS }, response.data.addBook)
     }
   })
+
+  const updateCache = (cache, query, addedBook) => {
+    const uniqByTitle = (b) => {
+      let seen = new Set()
+      return b.filter((item) => {
+        let k = item.title
+        return seen.has(k) ? false : seen.add(k)
+      })
+    }
+
+    cache.updateQuery(query, ({ allBooks }) => {
+      return {
+        allBooks: uniqByTitle(allBooks.concat(addedBook))
+      }
+    })
+  }
 
   const submit = async (event) => {
     event.preventDefault()
