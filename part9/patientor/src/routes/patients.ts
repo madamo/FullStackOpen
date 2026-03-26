e3930b5cde4aee31f288c0382ce146057bd94a34
madamo <mattadamo@gmail.com>
2026-03-25 21:33:57 -0700

    part9 9_11 working on returning patient json

--- part9/patientor/src/routes/patients.ts
+++ part9/patientor/src/routes/patients.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { Response } from 'express'
+import { patientService } from '../services/patientService';
+import { NonSensitivePatient } from '../types';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all patients!!');
+  res.send(patientService.getPatients());
 });
 
 export default router;
