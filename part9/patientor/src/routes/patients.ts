8d6bc629b2a4e6a22834fc5ac5f8922c08cbec8a
madamo <mattadamo@gmail.com>
2026-04-09 21:24:04 -0700

    part9 9_12

--- part9/patientor/src/routes/patients.ts
+++ part9/patientor/src/routes/patients.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+
 import express from 'express';
 //import { Response } from 'express'
 import patientService from '../services/patientService';
@@ -5,10 +8,16 @@ import patientService from '../services/patientService';
 
 const router = express.Router();
 
-//TO-DO: Create endpoint to add patients
-
 router.get('/', (_req, res) => {
   res.send(patientService.getNonSensitivePatients());
+});
+
+//TO-DO: Create endpoint to add patients
+router.post('/', (req, res) => {
+  console.log(req.body)
+  const newPatient = req.body;
+
+  res.send(newPatient);
 });
 
 export default router;
