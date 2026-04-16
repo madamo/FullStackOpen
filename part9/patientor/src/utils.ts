53ce09c15a781bfcc31ec6a63666be0e1f66fe0c
madamo <mattadamo@gmail.com>
2026-04-15 21:39:27 -0700

    part9 9_12

--- part9/patientor/src/utils.ts
+++ part9/patientor/src/utils.ts
@@ -1,1 +1,61 @@
 //TO-DO validate requests
+
+
+/* REFERENCE 
+import { Weather, NewDiaryEntry } from './types';
+
+const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
+  
+  console.log(object);
+
+  const newEntry: NewDiaryEntry = {
+    weather: parseWeather(object.weather),
+    visibility: 'great',
+    date: '2022-1-1',
+    comment: 'fake news'
+  };
+
+  //TO-DO: validate request with type guards
+
+  return newEntry;
+};
+
+const parseComment = (comment: unknown): string => {
+  if (!comment || !isString(comment)) {
+    throw new Error('Incorrect or missing comment');
+  }
+
+  return comment;
+};
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error('Incorrect or missing date: ' + date);
+  }
+
+  return date;
+};
+
+const parseWeather = (weather: unknown): Weather => {
+  if (!weather || !isString(weather) || !isWeather(weather)) {
+    throw new Error('Incorrect or missing weather: ' + weather);
+  }
+
+  return weather;
+};
+
+const isWeather = (param: string): param is Weather => {
+  return Object.values(Weather).map(v => v.toString()).includes(param);
+}
+
+
+export default toNewDiaryEntry;
+*/
